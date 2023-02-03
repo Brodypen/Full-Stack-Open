c3d8448b796412060ee187b46f37ffe1d66b23cb
Brody <brodypen7987@gmail.com>
2023-02-03 09:04:43 -0600

    Finish 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -13,14 +13,17 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      
-      <Content part={part2} exercises={exercises2}/>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercise3={exercises3}
+      />
 
-      <Content part={part3} exercises={exercises3}/>
-      
-      <Total exercises={exercises1 + exercises2 + exercises3}/>
+      <Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
