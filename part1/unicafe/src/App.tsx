d4fb72bba4500cd885c65a9bdfbabd6645fc4764
Brody <brodypen7987@gmail.com>
2023-02-04 15:16:21 -0600

    1.7

--- part1/unicafe/src/App.tsx
+++ part1/unicafe/src/App.tsx
@@ -1,13 +1,24 @@
 import React, {useState} from 'react';
-import './App.css';
 
 function App() {
-   const [good, setGood] = useState<Number>(0);
-   const [neutral, setNeutral] = useState<Number>(0);
-   const [bad, setBad] = useState<Number>(0);
+   const [good, setGood] = useState<number>(0);
+   const [neutral, setNeutral] = useState<number>(0);
+   const [bad, setBad] = useState<number>(0);
   return (
     <div className="App">
-        code here
+        
+        <h1>Give Feedback</h1>
+        <button onClick={() => setGood(good + 1)}>Good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+        <button onClick={() => setBad(bad + 1)}>Bad</button>
+        <h1>Statistics</h1>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>All: {good + neutral + bad}</p>
+        <p>Average: {(good - bad)/(good + neutral + bad)}</p>
+        <p>Positive: {(good/(good + neutral + bad))*100}%</p>
+
     </div>
   );
 }
