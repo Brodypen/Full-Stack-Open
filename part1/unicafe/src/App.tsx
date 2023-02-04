5584cf16d978f89aa1c7dc01c33df7aff3489c4c
Brody <brodypen7987@gmail.com>
2023-02-04 14:29:30 -0600

    1.6

---
+++ part1/unicafe/src/App.tsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+   const [good, setGood] = useState<Number>(0);
+   const [neutral, setNeutral] = useState<Number>(0);
+   const [bad, setBad] = useState<Number>(0);
+  return (
+    <div className="App">
+        code here
+    </div>
+  );
+}
+
+export default App;
