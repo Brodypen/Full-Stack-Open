fe4e211711ce3c2c1ebcc24dcc456dc0975712cd
Brody <brodypen7987@gmail.com>
2023-02-05 13:35:31 -0600

    2.2-2.5

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -6,8 +6,11 @@ const Total = ({parts}) => {
 
   return (
     <p>
-      Number of exercises{" "}
-      {parts.reduce((sum, part) => sum + part.exercises, 0)}
+      <b>
+        Total of{" "}
+        {parts.reduce((sum, part) => sum + part.exercises, 0)}
+        {" "}exercises
+      </b>
     </p>
   );
 }
