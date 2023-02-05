4998995c031cd5e4cf9e9dec131eb983999b1db3
Brody <brodypen7987@gmail.com>
2023-02-05 13:22:17 -0600

    2.1

---
+++ part2/courseinfo/src/components/Total.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Total = ({parts}) => {
+
+
+
+  return (
+    <p>
+      Number of exercises{" "}
+      {parts.reduce((sum, part) => sum + part.exercises, 0)}
+    </p>
+  );
+}
+
+export default Total
