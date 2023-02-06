b2edb5a9ede00cecf748391ccd22154c310e80b2
Brody <brodypen7987@gmail.com>
2023-02-06 12:34:45 -0600

    2.9

--- part2/phonebook/src/App.tsx
+++ part2/phonebook/src/App.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "040-123-4567" }]);
+    const [persons, setPersons] = useState([
+      { name: "Arto Hellas", number: "040-123456" },
+      { name: "Ada Lovelace", number: "39-44-5323523"},
+      { name: "Dan Abramov", number: "12-43-234345"},
+      { name: "Mary Poppendieck", number: "39-23-6423122"},
+    ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
 
   const addPerson = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -17,6 +23,7 @@ const App = () => {
     };
     setPersons(persons.concat(personObject));
     setNewName("");
+    setNewFilter("");
   };
 
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -25,10 +32,19 @@ const App = () => {
   const handleNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNewNumber(event.target.value);
   };
+  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewFilter(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with:{" "}
+        <input value={newFilter} onChange={handleFilterChange} />
+        <div>debug: {newFilter}</div>
+      </div>
+      <h3>Add a new</h3>
       <form onSubmit={(e) => addPerson(e)}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -40,9 +56,9 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <div>debug: {newName}</div>
+
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase())).map((person) => (
         <p key={person.name}>{`${person.name} ${person.number}`}</p>
       ))}
     </div>
