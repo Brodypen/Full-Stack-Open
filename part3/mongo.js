3e6bcc26dbea632b19074931be077f0918636fda
Brody <brodypen7987@gmail.com>
2023-02-24 15:45:08 -0600

    Finish 3.12

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
-
 if (process.argv.length < 3) {
   console.log("give password as argument");
   process.exit(1);
 }
-
+console.log(process.argv);
 const password = process.argv[2];
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb+srv://frostwizard7987:${password}@cluster0.xme8n24.mongodb.net/notesApp?retryWrites=true&w=majority`;
 
 mongoose.set("strictQuery", false);
 mongoose.connect(url);
@@ -20,11 +19,18 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model("Note", noteSchema);
 
 const note = new Note({
-  content: "HTML is Easy",
-  important: true,
+  content: "MongoDB is also cool",
+  important: false,
 });
 
-note.save().then((result) => {
-  console.log("note saved!");
-  mongoose.connection.close();
-});
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
+
+// Note.find({}).then((result) => {
+//   result.forEach((note) => {
+//     console.log(note);
+//   });
+//   mongoose.connection.close();
+// });
